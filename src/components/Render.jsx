55411437219bd7ecc70e14e1ea7870ff8b2cd33a
Krit Bhattarai <kkb62@scarletmail.rutgers.edu>
2023-08-29 15:24:48 -0400

    update according to client specs

--- src/components/Render.jsx
+++ src/components/Render.jsx
@@ -1,37 +1,191 @@
 import { GoogleMap,useJsApiLoader} from "@react-google-maps/api";
-import FetchMarks from "./FetchMarks";
 import "../style.css" ;
+import { useState,useEffect,useRef,useCallback } from "react";
+import Papa from "papaparse";
+
+
+let coordinates = [];
+const center = { lat: 40.7831, lng: -73.9712 };
+const markers = [];
+const out = [];
+const outputData = [];
 // Function to render Map
-function Render() {
+export default function Render() {
+
+  FetchMarks();
   
   // Loading Google Maps
-    const { isLoaded } = useJsApiLoader({
-      googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
-    });
+  const mapRef = useRef();
+   
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+  });
   
+  // Keeping the current reference of Google Map
+  const onLoad = useCallback((map) => (
+    mapRef.current = map
     
-    return (
-      <div className="App">
-        {!isLoaded ? (
-          <h1>Loading...</h1>
-        ) : (
-          <GoogleMap
-            id="map"
-            mapContainerClassName="map-container"
-            center={{ lat: 40.57852755, lng: -74.3276703 }}
-            zoom={13}
-            options={{mapId: process.env.REACT_APP_MAP_ID}}
-            onLoad={console.log("loaded")}
-          >
-          <FetchMarks />
-          </GoogleMap>
+    ),[]);
+
+  // Rendering here
+  return (
+    <div className="App">
+      <button id="export-btn" className="export-button" onClick={ExportData}>Export</button>
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          id="map"
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={10}
+          options={{mapId: process.env.REACT_APP_MAP_ID, disableDefaultUI:true}}
+          onLoad={onLoad}
           
-        )}
+        >
         
-      </div>
-    );
-    
+        < Mark map={mapRef.current}/>
+        </GoogleMap>
+        
+      )}
+      
+    </div>
+  );
+
+}
+
+
+
+
+function ExportData() {
+  
+  
+    fetch(
+      "https://script.google.com/macros/s/AKfycbyUrQBU7GRRx2w6dwwXnQRSsd5_prDQnYtakOlPrmIkp0W1YGXghwaqkY8ED81YdMCnIg/exec",
+      {
+        method: "POST",
+        body: JSON.stringify(outputData),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log(out);
+  }
+
+  
+   /*
+  // Geo Coder
+  const geocoder = new window.google.maps.Geocoder();
+  
+  for(let i in markers){
+    if (markers[i].position == null) { continue; }
+    // Reverse geocode
+    geocoder
+      .geocode({location: markers[i].position})
+      .then((response) => {
+        let address = response.results[0].formatted_address;
+        out.push(address);
+        });
+  };
+
+
+  console.log(out);
+ 
+  
+  
+  
+ 
+  */
+  
+ 
+
+
+// Function to place markers, takes current reference of the map
+function Mark({map}){
+
+  // infowindow
+  
+  
+  // states of count and positions clicked (to export)
+  const [count,setCount] = useState(0);
+  const [pos,setPos] = useState();
+  
+  const geocoder = new window.google.maps.Geocoder();
+
+  useEffect(() => {
+    pos && geocoder
+      .geocode({location: pos})
+      .then((response) => {
+        let address = response.results[0].formatted_address;
+        outputData.push(address);
+        });
+
+  });
+  // Loop through the data and place appropriate markers
+  for(let i in coordinates){
+
+    // marker
+    const marker = new window.google.maps.Marker({
+      position: coordinates[i],
+      map: map
+    });
+    markers.push(marker);
+
+  };
+
+  for (let i in markers){
+    const infowindow = new window.google.maps.InfoWindow();
+    // Listener for marker onClick!
+    markers[i].addListener("click",() => {
+      setCount(count+1);
+      setPos(markers[i].getPosition());
+      
+      infowindow.setContent(count.toString());
+      infowindow.open({
+        anchor: markers[i],
+        map: map
+      });
+    }); 
   }
   
-  export default Render;
+  
+}
+
+
+// Function to pull data
+
+function FetchMarks() {
+  
+  // State of data
+  const [data, setData] = useState();
+
+  // Using papaparse to import data
+  useEffect(() => {
+    Papa.parse(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vSrxMVx-5wgzKZxsZOlsl9H1lUfVpoq6eHrtllFWrZ0v1FyKJYbXHiIv7LklNGt-WBabjbMRYybrRRO/pub?output=csv",
+      {
+        download: true,
+        header: true,
+        complete: (results) => {
+          setData(results.data);
+        },
+      }
+    );
+  }, []);
+
+
+  // After data is downloaded, place it in the coordinates array
+  data?.map(({latitude, longitude }) =>
+    coordinates.push({
+      lat: parseFloat(latitude),
+      lng: parseFloat(longitude)
+    })
+  );
+
+}
 
